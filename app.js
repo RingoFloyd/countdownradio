3ab4fc26703b2b17b8bb1ac4f996e9751efb243d
RingoFloyd <65081992+RingoFloyd@users.noreply.github.com>
2021-04-18 22:54:59 +0200

    18/4/21

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+
 
 const homeStartingContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip";
 const homeHeading = "Countdown Radio";
@@ -17,6 +19,22 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb//:localhost27017/countdownradioDB",{ userNewUrlParser: true});
+
+const postSchema = new mongoose.Schema ({
+    title: String,
+    content: String
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+const post = new Post ({
+    title: "This is a tester.",
+    content: "This is to test that the database code is working. I honestly hope this works because I feel a little lost over here!"
+});
+
+post.save();
 
 app.get("/", function(req, res){
     res.render("home", {
